705f7a2bb4226dcd97564890b0c1972432198776
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2021-11-08 20:36:52 -0500

    ENH: Allow the issues to pages script to accept parameters
    Allow the issues to pages script to accept parameters.
    
    Document the purpose of the script.

--- issues_to_pages.py
+++ issues_to_pages.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Convert relevant issue data to Markdown files so that they can be used to
+populate the website project section.
+
+Usage:
+> python issues_to_pages.py "https://api.github.com/repos/brainhackorg/global2021/issues?per_page=100" "content/project"
+
+"""
+
+import argparse
 import json
 import os
 import requests
@@ -277,11 +287,28 @@ def save_project_data(website_project_data, path):
             json.dump(project[1], json_file, indent=2)
 
 
+def _build_arg_parser():
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=__doc__)
+
+    parser.add_argument("url", help="URL where to fetch issues.")
+    parser.add_argument(
+        "path",
+        help="Path where to write relevant issue data to Markdown files."
+    )
+
+    return parser
+
+
 def main():
 
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+
     # Request issues
-    url = "https://api.github.com/repos/brainhackorg/global2021/issues?per_page=100"
-    response = request_issues(url)
+    response = request_issues(args.url)
 
     # Check whether the response was successful
     success = check_issue_data_request(response)
@@ -295,8 +322,7 @@ def main():
     website_project_data = gather_website_project_data(issues)
 
     # Save the project data
-    path = "content/project"
-    save_project_data(website_project_data, path)
+    save_project_data(website_project_data, args.path)
 
 
 if __name__ == "__main__":
